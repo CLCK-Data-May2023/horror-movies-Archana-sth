5bba0a5b59d0ac9b14803135202b54ede790e9e0
Archana Khaiyarguli <archanasth765@gmail.com>
2023-10-02 23:11:09 -0400

    "horror movies assignment"

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,5 +1,7 @@
 -- Add -- Add your SQL here
 SELECT id AS "Movie_ID", name AS "Movie_Title", imdb_rating AS "Rating"
-FROM movies 
+FROM movies
 WHERE year < 1985
-ORDER BY imdb_rating DESC;
+ AND genre = "horror"
+ORDER BY imdb_rating DESC
+LIMIT 3;
